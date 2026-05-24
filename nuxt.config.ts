cfdd6b46c08cf3900f9858d03c13f0e71f44f5ff
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-05-24 21:06:10 +0700

    update base url for dev use

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  app: {
+    baseURL: '/website-v2/',
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/fonts',
