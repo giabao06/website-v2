b7c68fce57c4a7d536d8b96520494f1a3bac2a13
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-05-24 16:20:07 +0700

    tailwind css setup and other stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+// even there is an official TailwindCSS for Nuxt, do not use it -swyrin
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/fonts',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/i18n',
-    '@nuxtjs/tailwindcss'
-  ]
-})
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["./app/assets/css/main.css"],
+  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxtjs/i18n", "@nuxt/image"],
+
+  i18n: {
+    defaultLocale: "en-AU",
+  },
+
+  fonts: {
+    families: [
+      { name: "Quicksand", provider: "google" },
+      { name: "Playwright USA Traditional", provider: "google" },
+      { name: "Noto Sans", provider: "google" },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [],
+    },
+  },
+});
