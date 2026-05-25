c9e20061299cdfef8095bb1781a6c16e40eca32d
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-05-25 10:28:04 +0700

    use nuxt page router

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <header>
+        <NavBar />
+    </header>
+    <main>
+        <div class="min-w-7xl">
+            <NuxtPage />
+        </div>
+    </main>
+    <footer></footer>
 </template>
