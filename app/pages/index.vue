7b6e30f9d89d795d972035ba873af5165931b101
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-06-03 19:35:47 +0700

    fix the index page now that Gemini fixed Actions

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="h-screen z-0 bg-twokblue flex justify-center align-middle">
-        <div
-            className="flex flex-col gap-3 align-middle justify-center text-center"
-        >
-            <p class="font-title text-3xl">Coming soon on July...</p>
-            <p>Work in progress. Built using Nuxt and Vue.</p>
+    <div>
+        <div class="flex flex-col gap-3 justify-center text-center pt-72">
+            <h1 class="font-title text-4xl">Coming soon on July...</h1>
+            <p class="text-center">
+                Work in progress. Built using Nuxt and Vue.
+            </p>
         </div>
     </div>
 </template>
+
+<style>
+body {
+    background-color: var(--color-twokblue);
+}
+</style>
