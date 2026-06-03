bb015864bcb0094794da33bc520d51a8218da899
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-06-03 18:23:57 +0700

    build index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div className="min-w-full min-h-full bg-twokblue">
-        <p>test</p>
+    <div class="h-screen z-0 bg-twokblue flex justify-center align-middle">
+        <div
+            className="flex flex-col gap-3 align-middle justify-center text-center"
+        >
+            <p class="font-title text-3xl">Coming soon on July...</p>
+            <p>Work in progress. Built using Nuxt and Vue.</p>
+        </div>
     </div>
 </template>
