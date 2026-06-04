b5005e50e94214c0e8d6e5912e73768debca2919
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-06-04 16:22:20 +0700

    some test content (but I keep getting kicked to index)

---
+++ app/pages/stuff/[year]/[month]/[slug].vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute();
+const { year, month, slug } = route.params as {
+    year: string;
+    month: string;
+    slug: string;
+};
+const path = `/stuff/${year}/${month}/${slug}`;
+const { data: post } = await useAsyncData(`stuff-${path}`, () =>
+    queryCollection("stuff").path(path).first(),
+);
+</script>
+
+<template>
+    <!-- Render the blog post as Prose & Vue components -->
+    <ContentRenderer :value="post" />
+</template>
---
+++ content/stuff/2026/06/04-indev.md
@@ -0,0 +1,9 @@
+---
+title: Hello world!
+description: You found this page, surely you know the code?
+date: 2026-06-04
+---
+
+So, you found the in-development Content Management System (~~CNS~~ CMS).
+
+I hope you have a great day.
