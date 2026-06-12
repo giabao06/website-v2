6755f8d039b31ec31fe051263ecdd4df7fcf8827
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-06-12 16:04:27 +0700

    add blog entry component (not finished)

---
+++ app/components/StuffEntry.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps({
+    id: String,
+    title: String,
+    description: String,
+    date: String,
+    path: String,
+});
+</script>
+
+<template>
+    <NuxtLink
+        :to="path"
+        class="border border-black rounded-lg hover:border-white p-2"
+    >
+        <div class="grid grid-cols-2 mb-1">
+            <p class="text-left text-2xl">{{ title }}</p>
+            <p class="font-mono text-right">{{ date }}</p>
+        </div>
+        <div>
+            <p class="text-left">{{ description }}</p>
+        </div>
+    </NuxtLink>
+</template>
