c2e7dd8651550b019cd0c21110b6d21bd64800c1
Nguyễn Phạm Gia Bảo <hello@giabao06.xyz>
2026-06-12 16:04:43 +0700

    change content index page and some more testing content

--- app/pages/stuff/index.vue
+++ app/pages/stuff/index.vue
@@ -5,12 +5,23 @@ const { data: posts } = await useAsyncData("stuff", () =>
 </script>
 
 <template>
-    <div>
-        <h1>Blog</h1>
-        <ul>
-            <li v-for="post in posts" :key="post.id">
-                <NuxtLink :to="post.path">{{ post.title }}</NuxtLink>
-            </li>
-        </ul>
+    <div class="flex flex-col justify-center p-7 text-center bg-cafebrown">
+        <div id="header" class="flex flex-col gap-1.5">
+            <h1 class="font-quicksand font-extrabold text-5xl">
+                Stuff I wrote
+            </h1>
+            <p>The stuff I feel like writing from time to time</p>
+        </div>
+        <div id="posts" class="pt-4 flex flex-col gap-2 justify-center mx-auto">
+            <StuffEntry
+                v-for="post in posts"
+                :key="post.id"
+                :title="post.title"
+                :description="post.description"
+                :id="post.id"
+                :path="post.path"
+                :date="post.date"
+            />
+        </div>
     </div>
 </template>
---
+++ content/stuff/2026/06/12-multi-index-testing.md
@@ -0,0 +1,7 @@
+---
+title: Testing Multi-index
+description: You found this page, surely you know the code?
+date: 2026-06-12
+---
+
+hello again!
